c9df0026cb89837ab759ed578f3d571e77432b76
yujung7768903 <yu-jung31476@naver.com>
2021-09-05 01:36:53 +0900

    add 6025, 6026

---
+++ codeup_6025.py
@@ -0,0 +1,3 @@
+num1, num2 = input().split(" ")
+add = int(num1) + int(num2)
+print(add)
---
+++ codeup_6026.py
@@ -0,0 +1,11 @@
+##첫번째 해결 방법
+##add = 0
+##for i in range(2):
+##    num = input()
+##    add += float(num)
+##print(add)
+
+num1 = input()
+num2 = input()
+add = float(num1) + float(num2)
+print(add)
