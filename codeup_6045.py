5cc7d13726f1c281d05c47b01c17196bfa179771
yujung7768903 <yu-jung31476@naver.com>
2021-09-15 15:50:35 +0900

    add 6045, 6046

---
+++ codeup_6045.py
@@ -0,0 +1,6 @@
+i1, i2, i3 = input().split(" ")
+i1 = int(i1)
+i2 = int(i2)
+i3 = int(i3)
+total = i1 + i2 + i3
+print("%s %.2f"%(total, total / 3))
---
+++ codeup_6046.py
@@ -0,0 +1,3 @@
+i1 = input()
+i1 = int(i1)
+print(i1<<1)
