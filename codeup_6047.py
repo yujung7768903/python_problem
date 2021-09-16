d199ecfc4e90d23b8c54b0656cc2d983a80931d5
yujung7768903 <yu-jung31476@naver.com>
2021-09-16 22:19:12 +0900

    add 6047, 6048

---
+++ codeup_6047.py
@@ -0,0 +1,4 @@
+i1, i2 = input().split(" ")
+i1 = int(i1)
+i2 = int(i2)
+print(i1<<i2)
---
+++ codeup_6048.py
@@ -0,0 +1,7 @@
+i1, i2 = input().split(" ")
+i1 = int(i1)
+i2 = int(i2)
+if(i1 < i2) :
+    print("True")
+else :
+    print("False")
